e5c13de97706ec94394b798ee809fa970e074b64
Dilusha Sandeera Jayampathi <sandeerads@gmail.com>
2024-03-10 19:04:19 +0530

    change

--- src/main/java/com/ds/mongoTest/repository/StudentRepository.java
+++ src/main/java/com/ds/mongoTest/repository/StudentRepository.java
@@ -2,9 +2,11 @@ package com.ds.mongoTest.repository;
 
 import com.ds.mongoTest.bean.Student;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface StudentRepository extends MongoRepository<Student, String> {
 
     Optional<Student> findStudentByEmail(String email);
